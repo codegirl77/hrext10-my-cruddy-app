179c63fef10671b8b12cfd7bd1176fb03e65a518
codegirl77 <rachelklinkatsis@gmail.com>
2019-07-08 12:36:52 -0700

    added making pupies

--- app/app.js
+++ app/app.js
@@ -9,7 +9,6 @@
 
 const Dog = function(name, breed){
   this.name = name;
-  this.breed = breed;
   this.age = 1;
   this.hunger = 10;
   this.happiness = 100; 
@@ -17,10 +16,56 @@ const Dog = function(name, breed){
   this.traits = []; 
 };
 
+Dog.prototype.getOlder = function(){
+  this.age++;
+};
 
+Dog.prototype.feed = function(food){
+  if (this.hunger - food > 0) {
+    this.hunger -= food;
+  } else {
+    this.hunger = 0;
+  }
+};
 
+Dog.prototype.play = function(time){
+  // increase happiness by number of units of time 
+  if (this.happiness + time < 100){
+    this.happiness += time;
+  } else {
+    this.happiness = 100
+    this.getOlder()
+  }
+  //until zero energy
+  if (this.energy - time > 0){
+    this.energy -= time;
+  } else {
+    this.energy = 0;
+  }
+  this.getOlder();
+};
 
+Dog.prototype.nap = function(time){
+  if (this.energy + time < 100){
+    this.energy += time;
+  } else {
+    this.energy = 100;
+    this.getOlder();
+  }
 
+};
+
+Dog.prototype.addTrait = function(traitType){
+  this.traits.push(traitType);
+};
+
+var Puppy = function(name, parentOne, parentTwo){ 
+      Dog.call(this, name); 
+      this.traits = parentOne.traits[0]
+  }
+
+Puppy.prototype = Object.create(Dog.prototype); 
+Puppy.prototype.constructor = Puppy;
 
 
 
@@ -30,9 +75,17 @@ var createItem = function(key, value) {
    key['traits'] = [value]
    value = JSON.stringify(key)
    key = key.name
-   console.log(key)
 
   return window.localStorage.setItem(key, value);
+}
+
+var createPup = function(key, dogOneObj, dogTwoObj) {
+  var name = key
+  key = new Puppy(name,dogOneObj,dogTwoObj)
+  console.log(key)
+  var value = JSON.stringify(key)
+  key = key.name
+  return window.localStorage.setItem(key, value)
 }
 
 var updateItem = function(key, value) {
@@ -53,7 +106,7 @@ var showDatabaseContents = function() {
   for (var i = 0; i < window.localStorage.length; i++) {
     var key = window.localStorage.key(i);
     obj = JSON.parse(window.localStorage.getItem(key))
-    $('.dog-holder').append(`<div class="dog-description"><div><image src="adorable-animal-beagle-1345191.jpg"/></div><div> Name: ${key}</div><div>Personality: ${obj['traits']}</div></div>`)
+    $('.dog-holder').append(`<div class="dog-description"><div><image src="adorable-animal-beagle-1345191.jpg" id=${key} class="image"/></div><div> Name: ${key}</div><div>Personality: ${obj['traits']}</div></div>`)
   }
 }
 
@@ -63,6 +116,10 @@ var keyExists = function(key) {
 
 var getKeyInput = function() {
   return $('.key').val();
+}
+
+var getPupInput = function() {
+  return $('.pup').val();
 }
 
 var getValueInput = function() {
@@ -133,7 +190,31 @@ $(document).ready(function() {
     }
   })
 
-  $('.dog-description').on('click', function(){
-    console.log('hello')
+  var dogOneKey;
+  var dogOneObj;
+  var dogTwoKey;
+  var dogTwoObj;
+  var reference; 
+
+  $('.dog-holder').on('click', '.image', function(){
+    if(dogOneKey === undefined && dogTwoKey === undefined){
+     dogOneKey = $(this).attr("id");
+     dogOneObj = JSON.parse(window.localStorage.getItem(dogOneKey))
+    } else if (dogOneKey !== undefined && dogTwoKey === undefined){
+     dogTwoKey = $(this).attr("id");
+     dogTwoObj = JSON.parse(window.localStorage.getItem(dogTwoKey))
+     console.log(dogOneObj)
+     console.log(dogTwoObj);
+    } 
   })
+
+  $('.create-puppy').click(function() {
+    console.log('hello')
+          
+    var pup =  createPup(getPupInput(), dogOneObj, dogTwoObj);
+    console.log(pup)
+    showDatabaseContents();
+    resetInputs();
+
+    });
 })
